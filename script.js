e29c0fc24232cf70e2ea74a504c6854cf06fc67d
eduard1dev <car.edu.eng@gmail.com>
2021-07-13 21:16:05 -0300

    feat: add rotative banner

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const menu = document.querySelector('.menu'),
       contact_button = document.querySelector('#contact_button'),
       section_hero = document.querySelector("#sec-hero"),
       section_about = document.querySelector("#sec-about"),
-      social_buttons = document.querySelectorAll('.social-button')
+      social_buttons = document.querySelectorAll('.social-button'),
+      banner1_images = document.querySelectorAll('.banner1-image')
       
 
 let isMenuOpen = false;
@@ -33,9 +34,22 @@ menu_buttons.forEach(element => {
 });
 
 
+// Rotative banner function
+let i = 0
 
+setInterval(() => {
+    console.log(i)
+    banner1_images[i].classList.remove('image-selected')
 
+    const maxLength = banner1_images.length
+    if (i === maxLength - 1) {
+        i = 0
+    } else {
+        i++
+    }
 
+    banner1_images[i].classList.add('image-selected')
+}, 3500);
 
 
 // Smooth scroll to sections
